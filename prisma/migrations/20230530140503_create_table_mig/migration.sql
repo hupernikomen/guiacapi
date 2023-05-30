e13b2c32c6a5dd68fa5a479def5aac50405524f5
Wilson Ramos <hupcontato@gmail.com>
2023-05-30 11:05:26 -0300

    mig

--- prisma/migrations/20230530140503_create_table_mig/migration.sql
+++ prisma/migrations/20230530140503_create_table_mig/migration.sql
@@ -43,6 +43,7 @@ CREATE TABLE "lojas" (
     "referencia" TEXT,
     "latlng" JSONB,
     "entrega" BOOLEAN NOT NULL DEFAULT false,
+    "temaID" TEXT,
     "regiaoID" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
@@ -86,6 +87,8 @@ CREATE TABLE "produtos" (
 CREATE TABLE "categorias" (
     "id" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "categorias_pkey" PRIMARY KEY ("id")
 );
@@ -94,8 +97,18 @@ CREATE TABLE "categorias" (
 CREATE TABLE "regioes" (
     "id" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "regioes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "temas" (
+    "id" TEXT NOT NULL,
+    "cores" TEXT[],
+
+    CONSTRAINT "temas_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -115,6 +128,9 @@ ALTER TABLE "servicos" ADD CONSTRAINT "servicos_regiaoID_fkey" FOREIGN KEY ("reg
 
 -- AddForeignKey
 ALTER TABLE "profissionais" ADD CONSTRAINT "profissionais_servicoID_fkey" FOREIGN KEY ("servicoID") REFERENCES "servicos"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "lojas" ADD CONSTRAINT "lojas_temaID_fkey" FOREIGN KEY ("temaID") REFERENCES "temas"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "lojas" ADD CONSTRAINT "lojas_regiaoID_fkey" FOREIGN KEY ("regiaoID") REFERENCES "regioes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
