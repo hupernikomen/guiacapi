1f1c4c5884fb3b6d312d0e12244f7ae37111e5de
Wilson Ramos <hupcontato@gmail.com>
2023-06-01 08:21:56 -0300

    oferta number

--- dist/controllers/produto/AtualizaPdtController.js
+++ dist/controllers/produto/AtualizaPdtController.js
@@ -17,11 +17,13 @@ class AtualizaPdtController {
             const produtoID = req.query.produtoID;
             const { nome, descricao, preco, oferta, tamanho, categoriaID, } = req.body;
             const atualizaPdtService = new AtualizaPdtService_1.AtualizaPdtService();
+            let precoNumber = parseFloat(preco);
+            let ofertaNumber = parseFloat(oferta);
             const produto = yield atualizaPdtService.execute({
                 nome,
                 descricao,
-                preco,
-                oferta,
+                preco: precoNumber,
+                oferta: ofertaNumber,
                 tamanho,
                 categoriaID,
                 produtoID
--- src/controllers/produto/AtualizaPdtController.ts
+++ src/controllers/produto/AtualizaPdtController.ts
@@ -4,7 +4,7 @@ import { AtualizaPdtService } from '../../services/produto/AtualizaPdtService'
 class AtualizaPdtController {
     async handle(req: Request, res: Response) {
         const produtoID = req.query.produtoID as string
-        
+
         const {
             nome,
             descricao,
@@ -16,17 +16,20 @@ class AtualizaPdtController {
 
         const atualizaPdtService = new AtualizaPdtService();
 
+        let precoNumber = parseFloat(preco)
+        let ofertaNumber = parseFloat(oferta)
+
         const produto = await atualizaPdtService.execute({
             nome,
             descricao,
-            preco,
-            oferta,
+            preco: precoNumber,
+            oferta: ofertaNumber,
             tamanho,
             categoriaID,
             produtoID
         })
 
-        
+
 
         return res.json(produto)
     }
