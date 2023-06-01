ba71f1a334ac2bad8a4c3a952bf5ec5d6bed2e99
Wilson Ramos <hupcontato@gmail.com>
2023-06-01 05:31:00 -0300

    numbr

--- src/controllers/produto/CriaPdtController.ts
+++ src/controllers/produto/CriaPdtController.ts
@@ -4,29 +4,30 @@ import { CriaPdtService } from "../../services/produto/CriaPdtService";
 class CriaPdtController {
   async handle(req: Request, res: Response) {
     const criaPdtService = new CriaPdtService();
-    const lojaID = req.query.lojaID as string
+    const lojaID = req.loja_ID
+    // const lojaID = req.query.lojaID as string
     const {
       nome,
       descricao,
       preco,
       tamanho,
       categoriaID,
-     } =
+    } =
       req.body;
 
     if (!req.files) {
       throw new Error("Ops.. algo deu errado!");
     } else {
       const files = req.files;
-      
 
+      let precoNumber = parseFloat(preco)
 
       const produto = await criaPdtService.execute({
         nome,
         descricao,
-        preco,
+        preco: precoNumber,
         tamanho,
-        imagens:files,
+        imagens: files,
         categoriaID,
         lojaID,
       })
