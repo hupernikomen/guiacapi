b30a14b15545c8c4bad5d5dfb98d9d7fc73f57e6
Wilson Ramos <hupcontato@gmail.com>
2023-06-21 11:37:56 -0300

    ajustes

---
+++ prisma/migrations/20230621143743_create_table_mig/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `portfolio` on the `profissionais` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "profissionais" DROP COLUMN "portfolio";
