b6488fcbdb078b90522cc2440207c7f1ef68bd42
Wilson Ramos <hupcontato@gmail.com>
2023-10-02 19:15:43 -0300

    melhorias

--- src/services/person/ListarProfissionalService.ts
+++ src/services/person/ListarProfissionalService.ts
@@ -10,7 +10,8 @@ class ListarProfissionalService {
   async execute({ regionID }: PersonRquest) {
 
     const _person = await prismaClient.person.findMany({
-      where: { user: { status: true, regionID: regionID } },
+      where: { user: { status: true, OR: [{ regionID: regionID }, { regionID: { equals: "cb9085c6-439b-48da-8bc4-17ecd2800d4a" } }] } },
+      
       select: {
         id: true,
         avatar: true,
