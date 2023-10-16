540d0410be92a8e57e4afb15ee29f0fbd74248fb
Wilson Ramos <hupcontato@gmail.com>
2023-10-16 18:18:16 -0300

    criação banco payment

--- src/services/store/ListaLojasService.ts
+++ src/services/store/ListaLojasService.ts
@@ -16,9 +16,11 @@ class ListaLojasService {
             userID: true,
         }
 
+
+
         if (regionID === "cb9085c6-439b-48da-8bc4-17ecd2800d4a") {
             return await prismaClient.store.findMany({
-                where: { user: { status: true } },
+                where: { user: { status: true} },
                 select: select
 
             })
