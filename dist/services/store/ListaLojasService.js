402ec1a2d923f09d4608828886248e8e4ee14dd3
Wilson Ramos <hupcontato@gmail.com>
2023-10-20 08:33:48 -0300

    payment

--- dist/services/store/ListaLojasService.js
+++ dist/services/store/ListaLojasService.js
@@ -29,7 +29,7 @@ class ListaLojasService {
             };
             if (regionID === "cb9085c6-439b-48da-8bc4-17ecd2800d4a") {
                 return yield prisma_1.default.store.findMany({
-                    where: { user: { payment: { every: { expiration: { gt: today } } } } },
+                    where: { user: { payment: { every: { expiration: { gt: today }, paymentOf: "monthlyPayment" } } } },
                     select: select
                 });
             }
