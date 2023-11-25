113cf7a9e2406e1ea64069c5b44a6b8dab2270ec
Wilson Ramos <hupcontato@gmail.com>
2023-11-25 06:42:11 -0300

    Atualização

--- dist/services/store/AtualizaLojaService.js
+++ dist/services/store/AtualizaLojaService.js
@@ -14,12 +14,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AtualizaLojaService = void 0;
 const prisma_1 = __importDefault(require("../../prisma"));
+const aws_sdk_1 = __importDefault(require("aws-sdk"));
+let s3 = new aws_sdk_1.default.S3({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: 'us-east-1',
+});
 class AtualizaLojaService {
     execute({ userID, avatar, name, bio, address, district, reference, delivery, }) {
         return __awaiter(this, void 0, void 0, function* () {
             const _store = yield prisma_1.default.store.findFirst({ where: { userID } });
             if (!_store)
                 throw new Error("Ops, infelizmente não encontramos!");
+            const deletePromises = [];
+            let tempArray = [];
+            if (_store.avatar && Array.isArray(_store.avatar)) {
+                tempArray = _store.avatar.map((item) => {
+                    var params = { Bucket: process.env.BUCKETEER_BUCKET_NAME, Key: item.key };
+                    return s3.deleteObject(params).promise();
+                });
+            }
+            deletePromises.push(...tempArray);
+            yield Promise.all(deletePromises);
             const __store = yield prisma_1.default.store.updateMany({
                 where: { userID },
                 data: {
--- src/services/store/AtualizaLojaService.ts
+++ src/services/store/AtualizaLojaService.ts
@@ -1,5 +1,13 @@
 import prismaClient from "../../prisma"
 
+import AWS from 'aws-sdk';
+
+let s3 = new AWS.S3({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: 'us-east-1',
+});
+
 interface lojaDataRequest {
     userID: string,
     avatar: object,
@@ -27,6 +35,21 @@ class AtualizaLojaService {
         const _store = await prismaClient.store.findFirst({ where: { userID } })
 
         if (!_store) throw new Error("Ops, infelizmente não encontramos!");
+
+
+
+        const deletePromises = []
+        let tempArray = []
+        if (_store.avatar && Array.isArray(_store.avatar)) {
+            tempArray = _store.avatar.map((item: any) => {
+                var params = { Bucket: process.env.BUCKETEER_BUCKET_NAME, Key: item.key };
+                return s3.deleteObject(params).promise();
+            });
+        }
+
+        deletePromises.push(...tempArray)
+    
+        await Promise.all(deletePromises);
 
 
         const __store = await prismaClient.store.updateMany({
