e52dd6062b45f02c7095a8f3074c1af0d78fce0c
Wilson Ramos <hupcontato@gmail.com>
2023-12-12 15:06:34 -0300

    Atualização

--- dist/controllers/contact/ListaContatosController.js
+++ dist/controllers/contact/ListaContatosController.js
@@ -11,13 +11,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ListaContatosController = void 0;
 const ListaContatosService_1 = require("../../services/contact/ListaContatosService");
+const lodash_1 = require("lodash");
 class ListaContatosController {
     handle(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const listaContatosService = new ListaContatosService_1.ListaContatosService();
             const userID = req.query.userID;
             const _contact = yield listaContatosService.execute({ userID });
-            return res.json(_contact);
+            const shuffledResults = (0, lodash_1.shuffle)(_contact);
+            return res.json(shuffledResults);
         });
     }
 }
--- src/controllers/contact/ListaContatosController.ts
+++ src/controllers/contact/ListaContatosController.ts
@@ -1,16 +1,18 @@
-import { Request, Response } from 'express'
-import { ListaContatosService } from '../../services/contact/ListaContatosService'
+import { Request, Response } from 'express';
+import { ListaContatosService } from '../../services/contact/ListaContatosService';
+import { shuffle } from 'lodash';
 
 class ListaContatosController {
-    async handle(req: Request, res: Response) {
-        const listaContatosService = new ListaContatosService()
+  async handle(req: Request, res: Response) {
+    const listaContatosService = new ListaContatosService();
 
-        const userID = req.query.userID as string
+    const userID = req.query.userID as string;
 
-        const _contact = await listaContatosService.execute({ userID })
-        return res.json(_contact)
+    const _contact = await listaContatosService.execute({ userID });
 
-    }
+    const shuffledResults = shuffle(_contact);
+    return res.json(shuffledResults);
+  }
 }
 
-export { ListaContatosController }
+export { ListaContatosController };
